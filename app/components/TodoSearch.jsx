ea1c08a80dfcd8b32414cf72ff4e81d53d6ead9d
Greg Bacus <gbacus28@gmail.com>
2017-03-30 19:42:43 -0500

    Complete handleAddTodo

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -17,7 +17,7 @@ var TodoSearch = React.createClass({
         <div>
           <label>
             <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
-            Show Completed Todos
+            Show Completed TodoSearch
           </label>
         </div>
       </div>
