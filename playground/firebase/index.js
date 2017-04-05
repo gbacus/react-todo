81b64d8d1c136b99001c120aaa8486af9d675e1a
Greg Bacus <gbacus28@gmail.com>
2017-04-04 21:58:22 -0500

    added updates to firebase database

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -11,6 +11,51 @@ var config = {
 
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-  appName: 'Todo App'
+var firebaseRef = firebase.database().ref();
+
+firebaseRef.set({
+  app: {
+    name: "Todo App",
+    version: "1.0.0"
+  },
+  isRunning: true,
+  user: {
+    name: 'Greg',
+    age: 28
+  }
 });
+
+// firebaseRef.update({
+//   'app/name': "todo application",
+//   'user/name': 'Annie'
+// })
+
+firebaseRef.child('app').update({
+  name: "todo application2"
+})
+firebaseRef.child('user').update({
+  name: "Peter"
+})
+
+// firebaseRef.update({
+//   isRunning: false,
+//   'app/name': 'Todo Application'
+// });
+
+// firebaseRef.child('app').update({
+//   name: "todo application"
+// }).then(() => {
+//   console.log('Update worked!');
+// }, (e) => {
+//   console.log('Update failed');
+// })
+
+
+
+
+
+
+
+
+
+
