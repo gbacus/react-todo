37e25f82b7709bdec6910996793668294047c8e9
Greg Bacus <gbacus28@gmail.com>
2017-04-04 23:46:51 -0500

    All tests passing

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -75,12 +75,13 @@ describe('Actions', () => {
 
   });
 
-  it('should generate toggle todo action', () => {
+  it('should generate update todo action', () => {
     var action = {
-      type: 'TOGGLE_TODO',
-      id: 123
+      type: 'UPDATE_TODO',
+      id: 123,
+      updates: {completed: false}
     }
-    var res = actions.toggleTodo(action.id);
+    var res = actions.updateTodo(action.id, action.updates);
 
     expect(res).toEqual(action);
   })
