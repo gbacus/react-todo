52cc7670b6bb3bcb9fe47dbef7bf554237ab52ba
Greg Bacus <gbacus28@gmail.com>
2017-04-09 18:34:17 -0500

    Move firebase creds to env varibales

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,10 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyAPbiHo6bK-isB4ofPVUibbjv3DcyPSo5w",
-    authDomain: "greg-todo-app.firebaseapp.com",
-    databaseURL: "https://greg-todo-app.firebaseio.com",
-    projectId: "greg-todo-app",
-    storageBucket: "greg-todo-app.appspot.com",
-    messagingSenderId: "41012462468"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
 
   firebase.initializeApp(config);
